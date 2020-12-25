f35e4722baad61b0514873e79a3f33248294cb29
AlexVPer <alexander.vieirapereira@b-rayz.ch>
2020-12-25 15:20:17 +0100

    Updates

---
+++ precache-manifest.0ec3b7baeeec549b74ba69335586241d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cefab0ae1a160b359f861bf10dfe5216",
+    "url": "/AlexVPer/robofriends/index.html"
+  },
+  {
+    "revision": "939c29a12718c22b5acf",
+    "url": "/AlexVPer/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "8392ca77587d7677fb67",
+    "url": "/AlexVPer/robofriends/static/css/main.6a6e2c2c.chunk.css"
+  },
+  {
+    "revision": "939c29a12718c22b5acf",
+    "url": "/AlexVPer/robofriends/static/js/2.e7e24cc0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/AlexVPer/robofriends/static/js/2.e7e24cc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8392ca77587d7677fb67",
+    "url": "/AlexVPer/robofriends/static/js/main.44ed92c0.chunk.js"
+  },
+  {
+    "revision": "dc641992ea24f18bb5ad",
+    "url": "/AlexVPer/robofriends/static/js/runtime-main.7d7d6fc9.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/AlexVPer/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
